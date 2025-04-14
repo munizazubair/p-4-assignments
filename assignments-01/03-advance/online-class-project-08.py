0c83ec54596925ef7fc1d3c6a008c9b9263ab011
munizazubair <munizazubairkhan@gmail.com>
2025-04-15 01:01:40 +0500

    add

---
+++ assignments-01/03-advance/online-class-project-08.py
@@ -0,0 +1,18 @@
+# Streamlit Crash Course - YouTube Reference
+# Link: https://www.youtube.com/watch?v=Y6VdvNdNHqo&list=PLTsu3dft3CWhBViLMhL0Aqb75rkSz_CL-&index=16
+# Purpose: Documenting basics and understanding how to use Streamlit
+# Notes and practice from the video will be added below
+
+# Video Timestamps:
+# 00:00:00 - Intro
+# 00:00:35 - Introduction & Installation
+# 00:04:50 - Welcome Page
+# 00:06:40 - Documentation & App Gallery
+# 00:13:00 - Your First Streamlit App
+# 00:16:08 - Different Text Formats
+# 00:19:00 - Widgets
+# 00:33:13 - Displaying Data & Styling
+# 00:43:40 - Data Visualization on Web
+# 00:51:08 - Layout and Theme Options
+# 00:57:31 - Machine Learning Models on Web
+# 01:08:25 - Outro
